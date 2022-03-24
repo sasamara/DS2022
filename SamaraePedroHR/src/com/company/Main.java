52f7b058f2a6c911fcc5890f3c244a68fd04e48c
sasamara <samaramag2005@gmail.com>
2022-03-24 13:43:18 -0300

    Xadrez

---
+++ SamaraePedroHR/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double n1, n2;
+
+        n1 = 1;
+
+        for(int i=1; i<=64; i++){
+            n1 = n1*2;
+            System.out.println(n1);
+
+        }
+    }}
+
